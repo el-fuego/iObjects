83c6109ff1c041182178eb963b716b1a4b3ec463
webschik <webschik@gmail.com>
2014-01-24 19:16:12 +0200

    rename IMoney to INumber

--- IEmail.js
+++ IEmail.js
@@ -10,6 +10,10 @@
     var IEmail = function (val) {
         var data;
 
+        if (val != null && val != "") {
+            this.options.isDefault = false;
+        }
+
         if (!val) {
             return this;
         }
@@ -35,6 +39,10 @@
         name: "",
         domain: "",
 
+        options: {
+            isDefault: true
+        },
+
         /** @lends IEmail.prototype */
         _mask: (/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i),
 
@@ -44,6 +52,14 @@
          */
         validate: function () {
             return this._mask.test(this.value);
+        },
+
+        /**
+         *
+         * @returns {boolean}
+         */
+        isDefault: function () {
+            return !!this.options.isDefault;
         },
 
         /**
